c59ed9a5d0b90bf747e366790e74e442ccf45c9f
NguyenChung <chungforit@gmail.com>
2023-05-08 13:52:24 +0700

    fix error home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,17 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Services\Banner\BannerService;
+use App\Http\Services\Category\CategoryService;
+use App\Http\Services\Product\ProductService;
 
 class HomeController extends Controller
 {
+    protected $banner;
+    protected $category;
+    protected $product;
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(BannerService $banner, CategoryService $category,
+    ProductService $product)
     {
         $this->middleware('auth');
+        $this->banner = $banner;
+        $this->category = $category;
+        $this->product = $product;
     }
 
     /**
@@ -23,6 +34,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home', [
+            'title' => 'Shop Nước Hoa ABC',
+            'banners' => $this->banner->show(),
+            'categories' => $this->category->show(),
+            'products' => $this->product->get()
+        ]);
     }
 }
